9546af6dce9700e85672926c5349a8657cf7c6e2
Lukas Niederberger <niederbergerlukas@gmail.com>
2024-03-23 16:47:44 -0700

    Add creature live views

---
+++ lib/cypher_srd_web/live/creature_live/show.ex
@@ -0,0 +1,43 @@
+defmodule CypherSrdWeb.CreatureLive.Show do
+  use CypherSrdWeb, :live_view
+
+  alias CypherSrd.SrdServer, as: SRD
+  import CypherSrd.Util, only: [title_case: 1, printable: 1]
+
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  def handle_params(%{"creature" => creature}, _uri, socket) do
+    socket =
+      socket
+      |> assign(:creature, SRD.get_srd(:creatures, creature))
+
+    {:noreply, socket}
+  end
+
+  def sections() do
+    [
+     :motive,
+     :environment,
+     :health,
+     :damage,
+     :armor,
+     :movement,
+     :modifications,
+     :combat,
+     :interaction,
+     :use,
+     :intrusions
+    ]
+  end
+
+  def section_title(:damage) do
+    "Damage Dealth"
+  end
+  def section_title(section) do
+    section
+    |> Atom.to_string()
+    |> title_case()
+  end
+end
